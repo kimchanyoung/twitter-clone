937d206f75407647ae75472afed4289829d54c43
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-11-24 11:48:53 -0500

    Create follow associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  has_many :tweets
+  has_many :followings, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followees, through: :followings, source: :followee
+  has_many :followeds, class_name: 'Follow', foreign_key: 'followee_id', dependent: :destroy
+  has_many :followers, through: :followeds, source: :follower
 end
